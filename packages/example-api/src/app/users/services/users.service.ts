cece9ce64a37d8779b5c191336a47654c64fc957
Miguel Montoya <miguel@invorious.com>
2023-03-01 17:26:45 -0500

    feat: create register function on client side

--- packages/example-api/src/app/users/services/users.service.ts
+++ packages/example-api/src/app/users/services/users.service.ts
@@ -1,16 +1,25 @@
 import { IMetamaskService } from '@invorious/access-control';
 import { Injectable } from '@nestjs/common';
+import { DeepPartial } from 'typeorm';
 import { User } from '../entities/user.entity';
 @Injectable()
 export class UsersService implements IMetamaskService<User> {
   users: User[] = [
-    { id: 1, address: '0x1' },
-    { id: 2, address: '0x2' },
-    { id: 3, address: '0x3' },
     { id: 0, address: '0xEdca6B2e3b5DDDFf53297f3E188C2Ca6D51c587f' },
   ];
 
   findByAddress(address: string): User {
     return this.users.find((user) => user.address === address);
+  }
+
+  register(data: DeepPartial<User>) {
+    const userId = this.users[this.users.length - 1].id + 1;
+    const newUser = {
+      id: userId,
+      address: data.address,
+      ...data,
+    };
+    this.users.push(newUser);
+    return newUser;
   }
 }
