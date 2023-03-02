677a60087bb752dccdd6e7323cfbab57d33a3dc3
Miguel Montoya <miguel@invorious.com>
2023-03-02 17:43:11 -0500

    fix: move login message to client side from lib

--- packages/access-control/src/lib/strategies/metamask/types/metamask-service.ts
+++ packages/access-control/src/lib/strategies/metamask/types/metamask-service.ts
@@ -2,4 +2,5 @@ import { IMetamaskUser } from './metamask-user';
 
 export interface IMetamaskService<T extends IMetamaskUser> {
   findByAddress(address: string): T;
+  loginMessage: string;
 }
--- packages/example-api/src/app/users/services/users.service.ts
+++ packages/example-api/src/app/users/services/users.service.ts
@@ -1,16 +1,18 @@
 import { IMetamaskService } from '@invorious/access-control';
 import { Injectable } from '@nestjs/common';
 import { User } from '../entities/user.entity';
+import { LOGIN_MESSAGE } from '../interfaces/login-message';
 @Injectable()
 export class UsersService implements IMetamaskService<User> {
   users: User[] = [
-    { id: 1, address: '0x1' },
-    { id: 2, address: '0x2' },
-    { id: 3, address: '0x3' },
-    { id: 0, address: '0xEdca6B2e3b5DDDFf53297f3E188C2Ca6D51c587f' },
+    { id: 1, address: '0xEdca6B2e3b5DDDFf53297f3E188C2Ca6D51c587f' },
   ];
 
   findByAddress(address: string): User {
     return this.users.find((user) => user.address === address);
+  }
+
+  get loginMessage() {
+    return LOGIN_MESSAGE;
   }
 }
