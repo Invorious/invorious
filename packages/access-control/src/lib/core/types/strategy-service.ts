362fc98c1db7fc239463c0712f2264771e0cbb2e
Miguel Montoya <miguel@invorious.com>
2023-03-02 10:20:50 -0500

    feat: add register function de service interface

--- packages/access-control/src/lib/core/types/strategy-service.ts
+++ packages/access-control/src/lib/core/types/strategy-service.ts
@@ -2,4 +2,5 @@ import { DeepPartial } from 'typeorm';
 
 export interface IStrategyService<T> {
   register(data: DeepPartial<T>): Promise<T> | T;
+  update(data: DeepPartial<T>): Promise<T> | T;
 }
