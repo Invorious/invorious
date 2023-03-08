3375d5b5bace4055f310b91b8bbfc8cc4dddf4bf
Jesus Rojas <jesus@invorious.com>
2023-03-08 18:34:03 -0500

    feat: se added build with nx and push of tags in github

--- semver.bash
+++ semver.bash
@@ -1,23 +1,52 @@
 #! /bin/bash
 
-if [ -z "$1" ]; then
-  echo 'Library name is required' && exit
+semver=('major' 'minor' 'patch')
+packages=('access-control')
+type=''
+package=''
+
+while getopts "t:p:" flag; do
+  case "${flag}" in
+    p)
+      package=${OPTARG}
+      ;;
+    t)
+      type=${OPTARG}
+      ;;
+  esac
+done
+
+if [ $package == '' ] || [ $type == '' ]; then
+  echo "Flag p and t are required" && exit
 fi
 
-packages=('access-control')
-packageExist='no'
-
-for package in ${packages[@]}
+exist='no'
+for value in ${semver[@]}
 do
-  if [ $1 == $package ]; then
-    packageExist='yes'
+  if [ $type == $value ]; then
+    exist='yes'
     break
   fi
 done
 
-if [ $packageExist == 'no' ]; then
-  echo "Library $1 doesn't exist in array of packages inside semver.bash" && exit
+if [ $exist == 'no' ]; then
+  echo "Flag t shoulbe (major|minor|patch)" && exit
 fi
 
-cd "./packages/$1" && npm run release:patch -- -t=$1.v && cd "../../dist/packages/$1" && \
-  npm publish && echo "Library $1 updated with semver, builded and uploaded in npm"
+exist='no'
+for value in ${packages[@]}
+do
+  if [ $package == $value ]; then
+    exist='yes'
+    break
+  fi
+done
+
+if [ $exist == 'no' ]; then
+  echo "Library $package doesn't exist in array of packages inside semver.bash" && exit
+fi
+
+cd "./packages/$package" && npm run release:$type -- -t=$package.v && \
+  npx nx build $package && cd "../../dist/packages/$package" && \
+  npm publish && git push origin --tags  && \
+  echo "Library $package updated with semver, builded and uploaded in npm"
