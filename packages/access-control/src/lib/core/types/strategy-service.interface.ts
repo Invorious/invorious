7a1f269149419df8524a610865f2d1c329ae0b6d
Miguel-Invorious <102982715+Miguel-Invorious@users.noreply.github.com>
2023-03-14 13:59:59 -0500

    fix: implement delete users function
    * fix: move delete endpoint from local strategy to core controller
    
    * fix: rename deleteUser function to delete

--- packages/access-control/src/lib/core/types/strategy-service.interface.ts
+++ packages/access-control/src/lib/core/types/strategy-service.interface.ts
@@ -1,5 +1,5 @@
 export interface IStrategyService<T> {
-  deleteUser(id: number): Promise<T[]> | T;
+  delete(id: number): Promise<T[]> | T;
   update(id: number, data: Partial<T>): Promise<T> | T;
   register(data: Partial<T>): Promise<T> | T;
   findById(id: number): Promise<T> | T;
