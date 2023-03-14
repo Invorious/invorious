e7deb65159e3bb1280e4a0c0f1925e5657dcfc30
Miguel-Invorious <102982715+Miguel-Invorious@users.noreply.github.com>
2023-03-14 14:01:39 -0500

    fix: use user management on core rather on each strategy
    * fix: implement register as a global function
    
    * fix: remove registerByGoogle function
    
    * fix: implement global update users function
    
    * fix: remove registration from metamask service
    
    * fix: implement update and get endpoint on core controller
    
    * fix: save all object in user update
    
    * fix: implement delete users function 
    
    * fix: move delete endpoint from local strategy to core controller
    
    * fix: rename deleteUser function to delete

--- packages/access-control/src/lib/strategies/local/controllers/local-strategy.controller.ts
+++ packages/access-control/src/lib/strategies/local/controllers/local-strategy.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Param,
-  Post,
-  UseGuards,
-} from '@nestjs/common';
-
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from '../guards/local-auth.guard';
 import { LocalStrategyService } from '../services/local-strategy.service';
 import { ILocalStrategyControllerOptions } from '../types';
@@ -30,11 +22,6 @@ export function buildLocalStrategyController(
     @Post(defaultStrategyOptions.loginUrl)
     async login(@Body() credentials: IUsernameAndPassword) {
       return this.localStrategyService.login(credentials);
-    }
-
-    @Delete('delete/:id')
-    async delete(@Param('id') id: number) {
-      return this.localStrategyService.delete(Number(id));
     }
   }
 
