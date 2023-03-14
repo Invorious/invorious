c3952ea33b448138bd3006af82ce1fafe48382f3
Miguel Montoya <miguel@invorious.com>
2023-03-13 20:43:23 -0500

    fix: implement update and get endpoint on core controller

--- packages/access-control/src/lib/core/controllers/access-control-core.controller.ts
+++ packages/access-control/src/lib/core/controllers/access-control-core.controller.ts
@@ -1,11 +1,22 @@
-import { Controller, Post, Body, Inject } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Inject,
+  UseGuards,
+  Get,
+  Put,
+  Request,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { tokenUserService } from '../tokens';
+import { IJwtPayload } from '../types/jwt-payload.interface';
 import { IController } from '../types/nest-js';
 import { IStrategyService } from '../types/strategy-service.interface';
 
 export function buildAccessControlCoreController(): IController {
   @Controller('/auth')
-  class AccessControlCoreController<T> {
+  class AccessControlCoreController<T, K extends IJwtPayload> {
     constructor(
       @Inject(tokenUserService)
       private userService: IStrategyService<T>,
@@ -13,6 +24,18 @@ export function buildAccessControlCoreController(): IController {
     @Post('/register')
     async registerUser(@Body() user: Partial<T>) {
       return this.userService.register(user);
+    }
+
+    @UseGuards(AuthGuard('jwt'))
+    @Get('/me')
+    async getUser(@Request() req: { user: K }) {
+      return this.userService.findById(req.user.id);
+    }
+
+    @UseGuards(AuthGuard('jwt'))
+    @Put('/me')
+    async updateUser(@Request() req: { user: K }, @Body() user: Partial<T>) {
+      return this.userService.update(req.user.id, user);
     }
   }
   return AccessControlCoreController;
