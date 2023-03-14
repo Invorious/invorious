215f3adb63308a3baee795627d9fd2b4dce0d0cb
Miguel Montoya <miguel@invorious.com>
2023-03-14 13:43:16 -0500

    feat: create hook for local strategy login

---
+++ packages/access-control-front/src/lib/hooks/use-local-strategy.ts
@@ -0,0 +1,29 @@
+import { useHttpClient } from '@invorious/http-client-front';
+import { CreateAxiosDefaults } from 'axios';
+import { ILoginResponse } from '../types/login-response.interface';
+import { ILocalStrategy } from '../types/local-strategy.interface';
+
+export function useLocalStrategy(
+  config?: CreateAxiosDefaults,
+  token?: string,
+): ILocalStrategy {
+  const { post } = useHttpClient({
+    config,
+    jwtToken: token,
+  });
+  async function login<T extends ILoginResponse>(
+    username: string,
+    password: string,
+  ) {
+    const response = await post<T>('/login', {
+      username,
+      password,
+    });
+    localStorage.setItem('token', response.accessToken);
+    return response;
+  }
+
+  return {
+    login,
+  };
+}
