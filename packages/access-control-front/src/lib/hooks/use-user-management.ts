53d8d2fbf9f5a0db225267c3c2e2b5d9bb10f7f8
Miguel Montoya <miguel@invorious.com>
2023-03-15 17:03:47 -0500

    fix: set config type for user management hook

--- packages/access-control-front/src/lib/hooks/use-user-management.ts
+++ packages/access-control-front/src/lib/hooks/use-user-management.ts
@@ -1,7 +1,10 @@
 import { useHttpClient } from '@invorious/http-client-front';
+import { CreateAxiosDefaults } from 'axios';
 import { IUserManagement } from '../types/user-management.interface';
 
-export function useUserManagement(): IUserManagement {
+export function useUserManagement(
+  config?: CreateAxiosDefaults,
+): IUserManagement {
   const { post, requestError } = useHttpClient({
     config: {
       baseURL: 'api/auth',
