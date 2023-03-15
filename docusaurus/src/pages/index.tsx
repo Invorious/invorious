343c3b1c85ad473cb5a0e1b198ab9d31ee656a14
Jesus Rojas <jesus@invorious.com>
2023-03-15 12:53:28 -0500

    fix(docusaurus): solved problems in merge

--- docusaurus/src/pages/index.tsx
+++ docusaurus/src/pages/index.tsx
@@ -8,7 +8,7 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
@@ -28,18 +28,13 @@ function HomepageHeader() {
 }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Description will go into a meta tag in <head />"
+    >
       <HomepageHeader />
-<<<<<<<< HEAD:docusaurus/src/pages/index.tsx
-========
-      <main>
-        <HomepageFeatures />
-      </main>
->>>>>>>> main:docs/src/pages/index.tsx
     </Layout>
   );
 }
