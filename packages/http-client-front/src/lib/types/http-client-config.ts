3fde432e93289a02d2cd2b0ada36ced4ab324bee
Miguel Montoya <miguel@invorious.com>
2023-03-15 10:54:55 -0500

    fix: remove jwt interceptor from http client

--- packages/http-client-front/src/lib/types/http-client-config.ts
+++ packages/http-client-front/src/lib/types/http-client-config.ts
@@ -2,6 +2,5 @@ import { AxiosError, CreateAxiosDefaults } from 'axios';
 
 export interface IHttpClientConfig {
   config?: CreateAxiosDefaults;
-  jwtToken?: string | null;
   onError?: (error: AxiosError) => void;
 }
