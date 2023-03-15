7516071924e8e6201a22be7b3f81bb7341a9b384
Miguel Montoya <miguel@invorious.com>
2023-03-15 13:39:46 -0500

    fix: modify app.ts to only return react routing

--- packages/example-front/src/app/app.tsx
+++ packages/example-front/src/app/app.tsx
@@ -1,53 +1,17 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 
-import { Route, Routes, Link } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import LoginOptions from './modules/login-options/login-options';
+import Register from './modules/register/register';
 
 export function App() {
   return (
-    <>
-      <LoginOptions />
-
-      <div />
-
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              This is the generated root route.{' '}
-              <Link to="/page-2">Click here for page 2.</Link>
-            </div>
-          }
-        />
-        <Route
-          path="/page-2"
-          element={
-            <div>
-              <Link to="/">Click here to go back to root page.</Link>
-            </div>
-          }
-        />
-      </Routes>
-      {/* END: routes */}
-    </>
+    <Routes>
+      <Route path="/login" element={<LoginOptions />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="*" element={<Navigate to="/login" />} />
+    </Routes>
   );
 }
 
